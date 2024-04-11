8f58c42b18b15c876bb47f28e2ffbe4152209022
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-11 13:14:08 +0200

    UI Done !

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -10,7 +10,7 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        backgroundColor: kSecondaryColor,
+        backgroundColor: kPrimaryColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30),
         ),
@@ -24,6 +24,7 @@ class NotesView extends StatelessWidget {
         },
         child: const Icon(
           Icons.add,
+          color: Colors.black,
         ),
       ),
       body: const NotesViewBody(),
