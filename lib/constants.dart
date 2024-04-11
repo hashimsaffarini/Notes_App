a7ae0b8e1ec49764c4db64db6a2176c8a555384d
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-11 12:36:04 +0200

    Add custom text field widget and constants for colors - Added a custom text field widget with specific styling for the text field - Created constants for primary and secondary colors to maintain consistency across the app

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff62FCD7);
+const kSecondaryColor = Color.fromARGB(255, 73, 220, 186);
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -9,7 +10,7 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 30, 121, 196),
+        backgroundColor: kSecondaryColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30),
         ),
