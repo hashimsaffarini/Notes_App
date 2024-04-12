fdeb0fc219046bbfe916b98b1e7683dbe711ddeb
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-13 01:16:52 +0200

    Update Hive initialization and add Flutter Bloc to main

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
@@ -42,6 +45,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (form.currentState!.validate()) {
                 form.currentState!.save();
+                // Add note to the database
+                var note = NoteModel(
+                    title: title!,
+                    subTitle: subTitle!,
+                    date: DateTime.now().toString(),
+                    color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(note);
               } else {
                 setState(() {
                   autoValidateMode = AutovalidateMode.always;
